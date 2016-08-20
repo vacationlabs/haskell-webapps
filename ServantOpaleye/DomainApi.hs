9a922826a889d2d229f69d1583db1b59191822c4
Saurabh Nanda <saurabh@vacationlabs.com>
2016-08-21 02:53:04 +0530

    added todo for validations

--- ServantOpaleye/DomainApi.hs
+++ ServantOpaleye/DomainApi.hs
@@ -106,6 +106,25 @@ getProductList4 user filter = undefined
 -- it next to the field on the UI.
 --
 
+-- TODO: Figure out the correct way to parameterize a record with the validation
+-- status. Should it be a field within the record or something else?
+data ValidationState = NotValidated | Validated
+data ValidationResult = Invalid | Valid
+
+-- TODO: Product2 needs to be a record obviously. But I don't know how to make a
+-- record parameterized over something. Also, how do we represent an invalid
+-- user-input in Haskell? Something which may have missing fields? Does this
+-- mean that we need THREE records-types to hold essentially the same data?
+-- First for user-input (which needs to be most tolerant, missing fields, etc.).
+-- Second for validated input with all the constraints checked. And third for
+-- something that only the DB layer might require. Seems too much.
+--
+-- Or, should the user-validation be done by the 'controller' layer, i.e. the
+-- JSON parsing code. So, the JSON parsing results in either a valid
+-- domain-record, or it results in the error object,
+-- i.e. `Either ValidationErrors Product`
+data Product2 = Product2 ValidationState
+
 --
 -- Create a new discount code
 --
