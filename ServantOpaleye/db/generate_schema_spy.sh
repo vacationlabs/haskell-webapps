66b9d993ddbb275f11df7381269ae645a2255a97
Saurabh Nanda <saurabh@vacationlabs.com>
2016-10-10 16:46:18 +0530

    added java command to generate schema_spy docs

---
+++ ServantOpaleye/db/generate_schema_spy.sh
@@ -0,0 +1,1 @@
+java -jar ~/Downloads/schemaSpy_5.0.0.jar -t pgsql -host localhost -u servant_opaleye -p servant_opaleye -o schema_spy -db servant_opaleye -dp ~/Downloads/postgresql-9.4.1211.jar -s public
