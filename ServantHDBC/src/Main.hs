a582e3115cd832ff6ad7fcd76bf6afd81b5a4c0d
Jason Foutz <jfoutz@gmail.com>
2016-10-11 18:02:35 -0600

    auth api. maybe the cookie should be handled in WAI

--- ServantHDBC/src/Main.hs
+++ ServantHDBC/src/Main.hs
@@ -25,24 +25,41 @@ type TenantAPI = "tenants" :>
                           :<|> Capture "x" Int :> Get '[JSON] Tenant
                           :<|> Capture "x" Int :> "activate" :> Post '[JSON] Tenant)
 
-tenantNew :: ExceptT ServantErr IO String
+tenantNew :: ExceptT ServantErr IO Tenant
 tenantNew = return "new tenant"
 
-tenantGet :: Int -> ExceptT ServantErr IO String
+tenantGet :: Int -> ExceptT ServantErr IO Tenant 
 tenantGet x = return $ "get tenant " ++ show x
 
-tenantActivate :: Int -> ExceptT ServantErr IO String
+tenantActivate :: Int -> ExceptT ServantErr IO Tenant
 tenantActivate x = return $ "activating " ++ show x
+
+tenantHandlers = tenantNew :<|> tenantGet :<|> tenantActivate
 
 -- Authentication
 
+type Auth = String
+type AuthAPI = "sessions" :>
+                         ("new" :> Post '[JSON] Auth
+                          :<|> "refresh" :> Post '[JSON] Auth
+                          :<|> "destroy" :> Post '[JSON] Auth)
+
+authNew :: ExceptT ServantErr IO Auth
+authNew = return "should create a cookie"
+
+authRefresh :: ExceptT ServantErr IO Auth
+authRefresh = return "should refresh session timeout"
+
+authDestroy :: ExceptT ServantErr IO Auth
+authDestroy = return "should destroy the session"
+
+authHandlers = authNew :<|> authRefresh :<|> authDestroy
 
 
-
-type ShoppingCart = TenantAPI
+type ShoppingCart = TenantAPI :<|> AuthAPI
 
 server :: Server ShoppingCart
-server = tenantNew :<|> tenantGet :<|> tenantActivate
+server =  tenantHandlers :<|> authHandlers
 
 shoppingCart :: Proxy ShoppingCart
 shoppingCart = Proxy
