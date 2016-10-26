285a203cb665b5cf03fbb027b713266bbc9d92a0
Sandeep.C.R <sandeepcr2@gmail.com>
2016-10-26 19:24:11 +0000

    Ran hindent

--- SpockOpaleye/app/Main.hs
+++ SpockOpaleye/app/Main.hs
@@ -4,9 +4,7 @@ import Database.PostgreSQL.Simple
 import DomainApi
 
 main :: IO ()
-main = do
-  conn <- connect defaultConnectInfo {
-    connectDatabase = "haskell-webapps"
-  }
-  create_tenant conn get_tenant
-  return ()
+main = 
+  do conn <- connect defaultConnectInfo {connectDatabase = "haskell-webapps"}
+     create_tenant conn get_tenant
+     return ()
--- SpockOpaleye/src/DataTypes.hs
+++ SpockOpaleye/src/DataTypes.hs
@@ -1,34 +1,33 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 
-module DataTypes 
-  (
-   Tenant(..),
-   TenantStatus(..)
-  ) where
+module DataTypes
+  (Tenant(..)
+  ,TenantStatus(..))
+  where
 
 import Data.Text
-
 import qualified Data.Profunctor.Product.Default as D
 import Opaleye (Constant(Constant), PGText, Column, pgString)
 
-data TenantStatus = TenantStatusActive | TenantStatusInActive | TenantStatusNew
+data TenantStatus
+  = TenantStatusActive 
+  | TenantStatusInActive 
+  | TenantStatusNew 
 
-data Tenant = Tenant {
-  tenant_id::Int,
-  tenant_name::String,
-  tenant_firstname::String,
-  tenant_lastname::String,
-  tenant_email::String,
-  tenant_phone::String,
-  tenant_status::TenantStatus,
-  tenant_ownerid::Maybe Int,
-  tenant_backofficedomain :: String
-}
+data Tenant =
+  Tenant {tenant_id :: Int
+         ,tenant_name :: String
+         ,tenant_firstname :: String
+         ,tenant_lastname :: String
+         ,tenant_email :: String
+         ,tenant_phone :: String
+         ,tenant_status :: TenantStatus
+         ,tenant_ownerid :: Maybe Int
+         ,tenant_backofficedomain :: String}
 
 instance D.Default Constant TenantStatus (Column PGText) where
   def = Constant def'
-    where 
-    def' :: TenantStatus -> (Column PGText)
-    def' TenantStatusInActive = pgString "inactive"
-    def' TenantStatusActive = pgString "active"
-    def' TenantStatusNew = pgString "new"
+    where def' :: TenantStatus -> (Column PGText)
+          def' TenantStatusInActive = pgString "inactive"
+          def' TenantStatusActive = pgString "active"
+          def' TenantStatusNew = pgString "new"
