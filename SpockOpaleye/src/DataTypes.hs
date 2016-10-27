37a7da23248a994dd785541aa6e7d8d2f8202740
Sandeep.C.R <sandeepcr2@gmail.com>
2016-10-27 06:53:21 +0000

    Wrap data returned from db in respective data types

--- SpockOpaleye/src/DataTypes.hs
+++ SpockOpaleye/src/DataTypes.hs
@@ -9,41 +9,45 @@ module DataTypes
 import Data.Text
 import qualified Data.Profunctor.Product.Default as D
 import Database.PostgreSQL.Simple.FromField
-import Opaleye (Constant(Constant), PGText,
-    Column, pgStrictText, QueryRunnerColumnDefault(queryRunnerColumnDefault),
-    fieldQueryRunnerColumn)
+import Opaleye
+       (Constant(Constant), PGText, Column, pgStrictText,
+        QueryRunnerColumnDefault(queryRunnerColumnDefault),
+        fieldQueryRunnerColumn)
 
 data TenantStatus
-  = TenantStatusActive 
-  | TenantStatusInActive 
-  | TenantStatusNew deriving (Show)
+    = TenantStatusActive 
+    | TenantStatusInActive 
+    | TenantStatusNew 
+    deriving ((((Show))))
 
-data Tenant =
-  Tenant {tenant_id :: Int
-         ,tenant_name :: Text
-         ,tenant_firstname :: Text
-         ,tenant_lastname :: Text
-         ,tenant_email :: Text
-         ,tenant_phone :: Text
-         ,tenant_status :: TenantStatus
-         ,tenant_ownerid :: Maybe Int
-         ,tenant_backofficedomain :: Text} deriving (Show)
+data Tenant = Tenant
+    { tenant_id :: Int
+    , tenant_name :: Text
+    , tenant_firstname :: Text
+    , tenant_lastname :: Text
+    , tenant_email :: Text
+    , tenant_phone :: Text
+    , tenant_status :: TenantStatus
+    , tenant_ownerid :: Maybe Int
+    , tenant_backofficedomain :: Text
+    } deriving ((((Show))))
 
 instance D.Default Constant TenantStatus (Column PGText) where
-  def = Constant def'
-    where def' :: TenantStatus -> (Column PGText)
-          def' TenantStatusInActive = pgStrictText "inactive"
-          def' TenantStatusActive = pgStrictText "active"
-          def' TenantStatusNew = pgStrictText "new"
+    def = Constant def'
+      where
+        def' :: TenantStatus -> (Column PGText)
+        def' TenantStatusInActive = pgStrictText "inactive"
+        def' TenantStatusActive = pgStrictText "active"
+        def' TenantStatusNew = pgStrictText "new"
 
 instance FromField (TenantStatus) where
-  fromField f mdata =
-    return gender
-    where
-      gender = case mdata of
-        Just "active" -> TenantStatusActive
-        Just "inactive" -> TenantStatusInActive
-        Just "new" -> TenantStatusNew
+    fromField f mdata = return gender
+      where
+        gender = 
+            case mdata of
+                Just "active" -> TenantStatusActive
+                Just "inactive" -> TenantStatusInActive
+                Just "new" -> TenantStatusNew
 
 instance QueryRunnerColumnDefault PGText TenantStatus where
-  queryRunnerColumnDefault = fieldQueryRunnerColumn
+    queryRunnerColumnDefault = fieldQueryRunnerColumn
--- SpockOpaleye/src/DomainApi.hs
+++ SpockOpaleye/src/DomainApi.hs
@@ -1,31 +1,24 @@
-{-# LANGUAGE FlexibleInstances, FlexibleContexts, MultiParamTypeClasses,
-  OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts,
+  MultiParamTypeClasses, OverloadedStrings #-}
 
 module DomainApi
-  (create_tenant, read_tenants)
+  (create_tenant
+  ,read_tenants)
   where
 
 import Database.PostgreSQL.Simple (Connection)
 import DataTypes
 import OpaleyeDef
-import Opaleye (Column, Query, PGInt4, runInsertMany, queryTable, constant, pgStrictText, runQuery)
+import Opaleye
+       (Column, Query, PGInt4, runInsertMany, queryTable, constant,
+        pgStrictText, runQuery)
 import qualified Opaleye.PGTypes as P
 import GHC.Int
 import Data.Text
 
-
 create_tenant
   :: Connection -> Tenant -> IO GHC.Int.Int64
-create_tenant conn Tenant{
-  tenant_id = id,
-  tenant_name = name,
-  tenant_firstname = first_name,
-  tenant_lastname = last_name,
-  tenant_email = email,
-  tenant_phone = phone,
-  tenant_status = status,
-  tenant_ownerid = owner_id,
-  tenant_backofficedomain = bo_domain} = 
+create_tenant conn Tenant{tenant_id = id,tenant_name = name,tenant_firstname = first_name,tenant_lastname = last_name,tenant_email = email,tenant_phone = phone,tenant_status = status,tenant_ownerid = owner_id,tenant_backofficedomain = bo_domain} = 
   runInsertMany conn tenantTable $
   (return (constant id
           ,pgStrictText name
@@ -37,5 +30,23 @@ create_tenant conn Tenant{
           ,Nothing
           ,pgStrictText bo_domain))
 
-read_tenants :: Connection -> IO [(Int, Text, Text, Text, Text, Text, TenantStatus, Maybe Int, Text)]
-read_tenants conn = runQuery conn $ queryTable tenantTable
+read_tenants
+  :: Connection
+  -> IO (Maybe [Tenant])
+read_tenants conn = do
+  r <- runQuery conn $ queryTable tenantTable
+  return $ case r of
+    [] -> Nothing
+    rows -> Just $ fmap make_tenant rows
+  where
+    make_tenant :: (Int,Text,Text,Text,Text,Text,TenantStatus,Maybe Int,Text) -> Tenant
+    make_tenant (id, name, first_name, last_name, email, phone, status, owner_id, bo_domain) = Tenant { 
+      tenant_id = id,
+      tenant_name = name,
+      tenant_firstname = first_name,
+      tenant_lastname = last_name,
+      tenant_email = email,
+      tenant_phone = phone,
+      tenant_status = status,
+      tenant_ownerid = owner_id,
+      tenant_backofficedomain = bo_domain }
