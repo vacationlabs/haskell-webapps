c47ebec860aad1a091cae1e29af0dd42fb0745e8
meditans <meditans@gmail.com>
2016-11-11 12:15:43 +0100

    Better version of add user widget

--- UI/ReflexFRP/mockUsersRoles/mockClient/app/Pages/Edit.hs
+++ UI/ReflexFRP/mockUsersRoles/mockClient/app/Pages/Edit.hs
@@ -1,4 +1,7 @@
-{-# LANGUAGE OverloadedStrings, NoImplicitPrelude, RecursiveDo #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, NoMonomorphismRestriction   #-}
+{-# LANGUAGE OverloadedStrings, RankNTypes, RecursiveDo, TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes, NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# OPTIONS_GHC -fdefer-typed-holes #-}
@@ -8,10 +11,12 @@ module Pages.Edit where
 import ClassyPrelude
 import Control.Lens
 import Reflex.Dom
-
+import Prelude ()
 import MockAPI
 import Pages.Common
 import Utils
+
+import ReflexJsx
 
 import Reflex.Dom.Contrib.Widgets.DynamicList
 
@@ -25,6 +30,7 @@ editPage roleName roleAttrs = do
           lateralNavigation
           divClass "col-md-9" $ do
             sitePosition ["Account Settings", "Roles", "Edit role: " <> roleName]
+            dangerDiv
             newData <- form roleName roleAttrs
             divClass "form-group" $ do
               btn <- buttonClass "btn btn-primary" "Save"
@@ -34,11 +40,13 @@ editPage roleName roleAttrs = do
 form :: MonadWidget t m => RoleName -> RoleAttributes -> m (Dynamic t (RoleName, RoleAttributes))
 form roleName roleAttrs =
   el "form" $ do
-    newRoleName <- divClass "form-group" $ do
+    newRoleName <- divClass "form-group has-error" $ do
       elAttr "label" ("class"=:"control-label") $ text "Role name"
-      fmap _textInput_value . textInput $ def
+      newRoleName <- fmap _textInput_value . textInput $ def
         & textInputConfig_initialValue .~ roleName
         & textInputConfig_attributes   .~ constDyn ("class"=:"form-control")
+      elClass "span" "help-block" $ text "Role name can't be blank"
+      return newRoleName
     newRolePerms <- divClass "form-group" $ do
       elClass "label" "control-label" $ text "Permissions"
       divClass "row" $ do
@@ -46,28 +54,82 @@ form roleName roleAttrs =
         op <- permissionCheckboxes "Orders"   (roleAttrs^.rolePermission) (map OP [minBound..maxBound])
         up <- permissionCheckboxes "Users"    (roleAttrs^.rolePermission) (map UP [minBound..maxBound])
         return (mconcat [pp, op, up])
-    updatedUsers <- updateUsers (roleAttrs^.roleAssociatedUsers)
+    updatedUsers <- updateUsers' (roleAttrs^.roleAssociatedUsers)
     return $ liftA2 (,) newRoleName (RoleAttributes <$> newRolePerms <*> updatedUsers)
 
-updateUsers :: MonadWidget t m => Set User -> m (Dynamic t (Set User))
-updateUsers users = setFromList . map fst <$$>
-  (divClass "form-group" $ do
-    elClass "label" "control-label" $ text "Users with this role:"
-    do rec a <- el "ul" $
-             dynamicList renderUser snd (const never) moreUsers (setToList users)
-           moreUsers <- addAnotherUser
-       return a)
+-- updateUsers :: MonadWidget t m => Set User -> m (Dynamic t (Set User))
+-- updateUsers users = setFromList . map fst <$$>
+--   (divClass "form-group has-error" $ do
+--     elClass "label" "control-label" $ text "Users with this role:"
+--     do rec a <- el "ul" $
+--              dynamicList renderUser snd (const never) moreUsers (setToList users)
+--            moreUsers <- addAnotherUser
+--        return a)
 
-addAnotherUser :: MonadWidget t m => m (Event t User)
-addAnotherUser =
- divClass "row" $
-   divClass "col-lg-6 col-md-8 col-sm-8 col-xs-12" $
-     divClass "input-group" $ do
-       divClass "input-group-addon" $ el "span" $ text "Add another user"
-       userInput <- textInput $ def & (textInputConfig_attributes .~ constDyn ("class"=:"form-control"))
-       let user = User <$> userInput ^. textInput_value
-       addUser <- divClass "input-group-btn" $ buttonClass "btn btn-default" "Add"
-       return (tagPromptlyDyn user addUser)
+usersDynList :: MonadWidget t m => Set User -> Event t User -> m (Dynamic t (Set User))
+usersDynList users addAnotherUser = setFromList . map fst <$$> do
+  dynamicList renderUser snd (const never) addAnotherUser (setToList users)
+
+updateUsers' :: MonadWidget t m => Set User -> m (Dynamic t (Set User))
+updateUsers' users = do
+  rec
+    (newSet,rawInputDyn,addButton) <- [jsx|
+<div {...maybeErrorAttr}>
+    <label class="control-label">Users with this role</label>
+    <ul>
+        {usersDynList users validatedUserEvent}
+    </ul>
+    <div class="row">
+        <div class="col-lg-6 col-md-8 col-sm-8 col-xs-12">
+            <div class="input-group">
+                <div class="input-group-addon"><span>Add another user</span></div>
+                {textInputClassValue "form-control"}
+                <div class="input-group-btn">
+                    {buttonClass "btn btn-default" "Add"}
+                </div>
+            </div>
+        </div>
+    </div>
+    <span {...maybeHiddenAttr}>
+        That doesn't look like a valid user. Does the user exist?
+    </span>
+</div>|]
+    let userDyn = User <$> rawInputDyn
+        (_,validatedUserEvent) = fanEither $ tagPromptlyDyn (validateUser' <$> userDyn) addButton
+    eventualError <- updatedOnButton addButton noUserError (validateUser <$> userDyn)
+    let maybeHiddenAttr = bool ("class"=:"help-block") ("class"=:"help-block hidden")
+                        . (== noUserError) <$> eventualError
+        maybeErrorAttr = bool ("class"=:"form-group has-error") ("class"=:"form-group")
+                        . (== noUserError) <$> eventualError
+        helpMsg = userShapedMail <$> eventualError
+  return newSet
+
+updatedOnButton :: (MonadWidget t m) => Event t b -> a -> Dynamic t a -> m (Dynamic t a)
+updatedOnButton btn init dyn = holdDyn init (tagPromptlyDyn dyn btn)
+
+textInputClassValue :: MonadWidget t m => Text -> m (Dynamic t Text)
+textInputClassValue c = view textInput_value <$>
+  textInput (def & textInputConfig_attributes .~ constDyn ("class"=:c))
+
+isRight = either (const False) (const True)
+
+filterLefts :: (Reflex t) => Event t (Either a b) -> Event t a
+filterLefts ev = fforMaybe ev selectLeft
+  where
+    selectLeft (Left a)  = Just a
+    selectLeft (Right _) = Nothing
+
+filterRights :: (Reflex t) => Event t (Either a b) -> Event t b
+filterRights ev = fforMaybe ev selectRight
+  where
+    selectRight (Left _)  = Nothing
+    selectRight (Right a) = Just a
+
+type Markup = forall t m a. (MonadWidget t m) => m a -> m a
+
+nestedDivClasses :: MonadWidget t m => [Text] -> m a -> m a
+nestedDivClasses []     content = content
+nestedDivClasses (c:cs) content = divClass c $ nestedDivClasses cs content
 
 renderUser :: MonadWidget t m => Int -> User -> Event t User -> m (User, Event t ())
 renderUser _ u _ =
@@ -96,3 +158,14 @@ permissionCheckboxes groupName permissions groupToDisplay =
             text (toUserLabel p)
             return $ bool (mempty :: Set Permission) (singletonSet p) <$> temp1
     return temp
+
+dangerDiv :: MonadWidget t m => m ()
+dangerDiv = do
+  elAttr "div" ("class"=:"alert alert-danger" <> "role"=:"alert") $ do
+    el "span" $ do
+      text "Please fix the errors highlighted in "
+      el "strong" $ text "red "
+      text "below:"
+    el "ul" $ do
+      el "li" $ text "Error not related to a specific field #1, eg. you must select at least one permission for this role"
+      el "li" $ text "Error not related to a specific field #2"
