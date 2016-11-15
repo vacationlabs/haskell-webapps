47a04c4873b28f8995c28f6c1367731088d0cd50
meditans <meditans@gmail.com>
2016-11-15 12:12:57 +0100

    Added better abstraction for buttons and apis

--- UI/ReflexFRP/reflex-ui-elements/src/UIElements.hs
+++ UI/ReflexFRP/reflex-ui-elements/src/UIElements.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE OverloadedStrings #-}
-
-module UIElements (disabledEnabledButton) where
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -fdefer-typed-holes #-}
+module UIElements (apiButton, disabledEnabledButton) where
 
 import Reflex
 import Reflex.Dom
@@ -8,10 +13,29 @@ import Data.Text
 import Data.Monoid
 import Data.Map (Map)
 
+apiButton :: MonadWidget t m
+          => Text                          -- ^ Text of the button
+          -> Map AttributeName Text        -- ^ Initial attributes for the button
+          -> (Event t () -> m (Event t r)) -- ^ Function transforming a click in an event a la servant-reflex
+          -> m (Event t r)                 -- ^ The result value of the api call
+apiButton label initialAttr f = do
+  rec (e, _) <- element "button" conf (text label)
+      let click = domEvent Click e
+      apiResponse <- f (() <$ click)
+      let conf = def & elementConfig_initialAttributes .~ initialAttr
+                     & elementConfig_modifyAttributes  .~ mergeWith (\_ b -> b)
+                           [ const disabled <$> click
+                           , const enabled  <$> apiResponse ]
+  return apiResponse
+ where
+  disabled = fmap Just initialAttr <> "disabled" =: Just "true"
+  enabled  = fmap Just initialAttr <> "disabled" =: Nothing
+
+--------------------------------------------------------------------------------
+-- Old code
 
 -- | This function constructs a button that has the enable or disable attribute
 --   set when two events fire.
-
 disabledEnabledButton :: DomBuilder t m
                       => Event t ()    -- ^ The event on which the button is enabled
                       -> Event t ()    -- ^ The event on which the button is disabled
@@ -22,11 +46,10 @@ disabledEnabledButton enable disable text initialAttr = styledButton conf text
   where
     conf = def & elementConfig_initialAttributes .~ initialAttr
                & elementConfig_modifyAttributes  .~ mergeWith (\_ b -> b)
-                   [ const disableAttr <$> disable
-                   , const enableAttr  <$> enable ]
-    disableAttr = fmap Just initialAttr <> "disabled" =: Just "true"
-    enableAttr  = fmap Just initialAttr <> "disabled" =: Nothing
-
+                   [ const disabled <$> disable
+                   , const enabled  <$> enable ]
+    disabled = fmap Just initialAttr <> "disabled" =: Just "true"
+    enabled  = fmap Just initialAttr <> "disabled" =: Nothing
 
 styledButton :: DomBuilder t m => ElementConfig EventResult t m -> Text -> m (Event t ())
 styledButton conf t = do
