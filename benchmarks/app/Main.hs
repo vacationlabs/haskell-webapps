ba92f4fe0a6a9249cc9dd2d9da425d0931dfb8d9
Sandeep.C.R <sandeepcr2@gmail.com>
2016-12-01 11:48:46 +0000

    Added a two way join

--- benchmarks/app/Main.hs
+++ benchmarks/app/Main.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE Arrows                #-}
+
 module Main where
 
 import Opaleye
-import Data.Profunctor.Product (p3)
+import Data.Profunctor.Product (p3, p4)
 
 import Database.PostgreSQL.Simple
 
@@ -9,6 +11,7 @@ import GHC.Int
 
 import Criterion.Main
 import Criterion.Types (Config(..))
+import Control.Arrow
 
 userTable :: Table 
   (Maybe (Column PGInt4), Column PGText, Column PGText) 
@@ -19,32 +22,55 @@ userTable = Table "users" (p3 (
   required "email"
   ))
 
+tenantTable :: Table 
+  (Maybe (Column PGInt4), Column PGText, Column PGText, Column PGInt4) 
+  (Column PGInt4, Column PGText, Column PGText, Column PGInt4)
+tenantTable = Table "tenants" (p4 (
+  optional "id",
+  required "name",
+  required "email",
+  required "owner_id"
+  ))
+
 getRows :: Connection -> IO [(Int, String, String)]
 getRows conn = do
   runQuery conn $ queryTable userTable
 
 insertRow :: Connection -> (Maybe Int, String, String) -> IO Int64
 insertRow conn u = do
-  conn <- connect defaultConnectInfo { connectDatabase = "scratch"}
   runInsertMany conn userTable [(constant u)] 
 
 insertRowReturning :: Connection -> (Maybe Int, String, String) -> IO [(Int, String, String)]
 insertRowReturning conn u = do
-  conn <- connect defaultConnectInfo { connectDatabase = "scratch"}
   runInsertManyReturning conn userTable [(constant u)] (\x -> x)
 
 updateRowReturning :: Connection -> (Maybe Int, String, String) -> IO [(Int, String, String)]
 updateRowReturning conn row = do
   runUpdateReturning conn userTable (\_ -> constant row) (\(id, _, _) -> id .== (pgInt4 1)) (\x -> x)
 
--- export LC_ALL=C.UTF-8
--- You may have to run this command in your terminal if you get a char encoding/display error
-main = IO ()
-main = do
-  conn <- connect defaultConnectInfo { connectDatabase = "scratch"}
+twowayJoin :: Connection -> IO [(String, String, String, String)]
+twowayJoin conn = do
+  runQuery conn $ proc () ->
+    do
+      (u_id, u_name, u_email) <- queryTable userTable -< ()
+      (t_id, t_name, t_email, t_uid) <- queryTable tenantTable -< ()
+      restrict -< (t_uid .== u_id)
+      returnA -< (u_name, u_email, t_name, t_email)
+
+opaleye_benchmark :: Connection -> IO ()
+opaleye_benchmark conn = do
+  conn <- connect defaultConnectInfo { connectDatabase = "benchmark"}
   defaultMainWith defaultConfig {resamples = 10000} [
       bench "getRows" $ nfIO $ getRows conn
     , bench "insertRow" $ nfIO $ insertRow conn (Nothing, "Max", "max@mail.com")
     , bench "insertRowReturning" $ nfIO $ insertRowReturning conn (Nothing, "Max", "max@mail.com")
-    ,bench "updateRowReturning" $ nfIO $ updateRowReturning conn (Nothing, "Bob", "max@mail.com")
+    , bench "updateRowReturning" $ nfIO $ updateRowReturning conn (Nothing, "Bob", "max@mail.com")
+    , bench "twowayJoin" $ nfIO $ twowayJoin conn 
     ]
+
+-- export LC_ALL=C.UTF-8
+-- You may have to run this command in your terminal if you get a char encoding/display error
+main :: IO ()
+main = do
+  conn <- connect defaultConnectInfo { connectDatabase = "benchmark"}
+  opaleye_benchmark conn
