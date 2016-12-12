1c9153dbe43726f0d35211fe8f4a0d5f4256eb97
meditans <meditans@gmail.com>
2016-12-12 17:21:38 +0100

    Added optional zipping, as we're not serving the uncompressed file
    The correct results will be discussed as a server implementation is added, in
    the next section. Should we minimize the library usage, though?

--- UI/ReflexFRP/starterApp/deploy.sh
+++ UI/ReflexFRP/starterApp/deploy.sh
@@ -10,3 +10,6 @@ cp -r $(stack path --local-install-root --stack-yaml=stack-ghcjs.yaml)/bin/start
 # Minifying all.js file using the closure compiler, and removing unnecessary files
 cd js
 ccjs all.js --compilation_level=ADVANCED_OPTIMIZATIONS > all.min.js
+
+# OPTIONAL: gzipping, to see the actual transferred size of the executable
+atool -a all.min.js.tar.gz all.min.js
