75ba0cf7d2e9d823e5686ffe0f41afeb1ecffd39
meditans <meditans@gmail.com>
2016-12-12 17:09:29 +0100

    Add deployment file handling closure compilation

---
+++ UI/ReflexFRP/starterApp/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Compiling with ghcjs
+stack build --stack-yaml=stack-ghcjs.yaml
+
+# Moving the generated files to the js folder
+mkdir -p js
+cp -r $(stack path --local-install-root --stack-yaml=stack-ghcjs.yaml)/bin/starterApp.jsexe/all.js js/
+
+# Minifying all.js file using the closure compiler, and removing unnecessary files
+cd js
+ccjs all.js --compilation_level=ADVANCED_OPTIMIZATIONS > all.min.js
