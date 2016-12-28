3c7349375457122c7e081d855fd27b67f4a6ed3c
Sandeep.C.R <sandeepcr2@gmail.com>
2016-12-28 08:47:14 +0000

    Updated to use nightly resolver (for latest version of Opaleye)

--- ServantOpaleye2/src/Endpoints/Authentication.hs
+++ ServantOpaleye2/src/Endpoints/Authentication.hs
@@ -11,7 +11,6 @@ module Endpoints.Authentication  where
 import           Servant
 import           AppM
 import           Data.Text
-import           Data.ByteString
 import           UserApi
 import           Control.Monad.IO.Class
 import           AppCore
@@ -22,10 +21,10 @@ import Servant.Server.Experimental.Auth.Cookie
 type instance AuthCookieData = CookieData
 
 type Type =
-        "login" :> ReqBody '[JSON] LoginInfo :> Post '[JSON] (Headers '[Header "set-cookie" ByteString] String)
+        "login" :> ReqBody '[JSON] LoginInfo :> Post '[JSON] (Headers '[Header "set-cookie" EncryptedSession] String)
    :<|> "protected" :> AuthProtect "cookie-auth" :> Get '[JSON] String
 
-login :: AuthCookieSettings -> RandomSource -> ServerKey -> LoginInfo -> AppM (Headers '[Header "set-cookie" ByteString] String)
+login :: AuthCookieSettings -> RandomSource -> ServerKey -> LoginInfo -> AppM (Headers '[Header "set-cookie" EncryptedSession] String)
 login ac rs sk (LoginInfo uname pword) = do
   authResult <- (authenticateUser uname pword)
   case authResult of
@@ -36,7 +35,7 @@ login ac rs sk (LoginInfo uname pword) = do
         (CookieData (user ^. id) [])
         login'
       ) 
-    _ -> return $ addHeader "Unauthenticated" login'
+    _ -> return $ addHeader (EncryptedSession "Unauthenticated") login'
   where 
     login' :: String
     login' = "test"
